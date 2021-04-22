3997160f8d6d8482280e645e37d351e0925629a8
jhrfadhil <77342457+jhrfadhil@users.noreply.github.com>
2021-04-22 17:34:04 +0700

    Activate slider and parralax

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
+// Side nav
 const sideNav = document.querySelectorAll(".sidenav");
 M.Sidenav.init(sideNav);
+
+// Slider
+const slider = document.querySelectorAll(".slider");
+M.Slider.init(slider, {
+  indicators: false,
+  height: 600,
+  transition: 600,
+  interval: 3000,
+});
+
+// Parralax
+const parallax = document.querySelectorAll(".parallax");
+M.Parallax.init(parallax);
